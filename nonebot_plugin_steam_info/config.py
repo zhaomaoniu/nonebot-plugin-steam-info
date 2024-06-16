0a4040f690c538103d1762d638304a8a65301b8b
Gitai <55650833+zhaomaoniu@users.noreply.github.com>
2024-06-17 01:44:18 +0800

    🐛 change proxy to optional

--- nonebot_plugin_steam_info/config.py
+++ nonebot_plugin_steam_info/config.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class Config(BaseModel):
     steam_api_key: str
-    proxy: str = None
+    proxy: Optional[str] = None
     steam_request_interval: int = 300  # seconds
     steam_broadcast_type: str = "part" # all, part, none
